b79c763652e8616bedac24083624b994070b7579
R Steiner <r.steiner@uconn.edu>
2016-12-29 14:33:39 -0500

    Version 0.1.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="wordutils",
-      version="0.1.0",
+      version="0.1.1",
       description="Utilities for research involving words.",
       keywords=[
             "psycholinguistics",
