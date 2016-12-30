57ef6a9498c90e222c5bb4e4fd03f856a8c6daf8
R Steiner <r.steiner@uconn.edu>
2016-12-29 21:19:49 -0500

    Update setup.py with correct link to tarball.

--- setup.py
+++ setup.py
@@ -1,21 +1,23 @@
 from setuptools import setup
 
+ver = "1.1.1"
 setup(name="lexlib",
-      version="1.1.0",
+      version=ver,
       description="Utilities for research involving words.",
       keywords=[
-            "psycholinguistics",
-            "research",
-            "words",
-            "language",
-            "linguistics",
-            "psychology",
-            "cognitive",
-            "cognitive science",
-            "cogsci"
+            "psycholinguistics",\
+            "research",\
+            "words",\
+            "language",\
+            "linguistics",\
+            "psychology",\
+            "cognitive",\
+            "cognitive science",\
+            "cogsci"\
             ],
       url="https://github.com/cranndarach/lexlib",
-      download_url="https://github.com/cranndarach/lexlib/tarball/0.1.0",
+      download_url="https://github.com/cranndarach/lexlib/tarball/{}".format(
+            ver),
       author="R. J. Steiner",
       author_email="r.steiner@uconn.edu",
       license="MIT",
