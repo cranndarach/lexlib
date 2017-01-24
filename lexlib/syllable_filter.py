a30d8edd64e3e10134ccb3b5f7bc462cd05eb80b
R. Steiner <r.steiner@uconn.edu>
2017-01-24 14:31:26 -0500

    Neaten syllable filter, add syllable counter for lists.

--- lexlib/syllable_filter.py
+++ lexlib/syllable_filter.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
-"""module: syllable_filter
+"""module: syllables
 author: R Steiner
 license: MIT, copyright (c) 2016 R Steiner
-description: Functions to filter words with desired number of syllables
-from a corpus.
+description: Functions for working with syllables: currently counting
+and filtering a corpus by number of syllables.
 """
 
 
@@ -12,6 +12,8 @@ def __decorator(fnc):
     return fnc
 
 
+# Does this have a purpose anymore, or is it a relic from when this was a class?
+# Might be time to deprecate...
 def __load_file(filepath):
     with open(filepath, 'r') as f:
         corpus = [word[:-1] for word in f]
@@ -21,18 +23,41 @@ load_corpus = __decorator(__load_file)
 load_vowels = __decorator(__load_file)
 
 
-def __count_syllables(word, vowels, sep=None):
+def nsyll_word(word, vowels, sep=None):
     """
     Returns the number of syllables (vowels) in a word.
 
     Arguments:
     word -- the word to be analyzed
+    vowels -- a list of vowels used in the word list's alphabet.
+    sep -- string to use as the delimiter for separating phonemes in the
+    words in the list. default: None (separate into individual characters)
     """
     counter = 0
+    # This actually makes it safe to use '' as the delimiter, because that will
+    # make `if not sep` return True.
     phonemes = list(word) if not sep else word.split(sep)
     for phoneme in phonemes:
         counter += 1 if phoneme in vowels else 0
     return counter
+
+
+def nsyll_list(words, vowels, sep=None):
+    """
+    Counts the number of syllables for a list of words. Returns a list
+    of (word, nsyll) pairs.
+
+    Arguments:
+    words -- a list of words whose syllables will be counted.
+    vowels -- a list of vowels used in the word list's alphabet.
+    sep -- string to use as the delimiter for separating phonemes in the
+    words in the list. default: None (separate into individual characters)
+    """
+    output = []
+    for w in words:
+        nsyll = nsyll_word(w, vowels, sep)
+        output.append((w, nsyll))
+    return output
 
 
 def filter_words(corpus, vowels, nsyll, sep=None):
@@ -48,5 +73,6 @@ def filter_words(corpus, vowels, nsyll, sep=None):
     words in the corpus. default: None (separate into individual characters)
     """
     nsyll = [nsyll] if type(nsyll) == int else nsyll
-    match = list(filter(lambda w: __count_syllables(w) in nsyll, corpus))
+    match = list(filter(lambda w: nsyll_word(w, vowels, sep) in
+                        nsyll, corpus))
     return match
