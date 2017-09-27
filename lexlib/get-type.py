c328baebd4c0a359be11d793cea94198dfc63786
R Steiner <r.steiner@uconn.edu>
2017-09-27 13:44:44 -0400

    Adjust get_neighbor_types() to handle dict of neighbors.

--- lexlib/get-type.py
+++ lexlib/get-type.py
@@ -1,21 +1,28 @@
 #!/usr/bin/env python3
 
-#import sys
-# import numpy as np
-
-# given a file organized with lines containing a target word and a neighbor
-# wordlist = np.loadtxt('types-test.txt',dtype=str)
-#wordlist = sys.argv[1]
 
 def get_neighbor_types(neighbor_dict):
-    types = {}
-    base_words = neighbor_dict.keys()
-    for i, j in wordlist: # where i is the target word and j is the neighbor
-        if len(i) == len(j): # if they are the same length, the change was a substitution
-            types.append([i,j,'S'])
-        elif len(i) > len(j): # if the target is longer than the neighbor, the change was a deletion
-            types.append([i,j,'D'])
-        elif len(i) < len(j): # if the target is shorter than the neighbor, the change was an addition
-            types.append([i,j,'A'])
-        else: # just to be sure
-            bad.append([i,j])
+    """
+    Takes a dict of neighbors (where a key is a "target" word and its value is a
+    list of all of its neighbors) and returns a list of (word1, word2,
+    relationship) triples, where `relationship` is one of "deletion,"
+    "addition," "substitution," or "unknown".
+
+    arguments:
+        neighbor_dict -- a dict whose keys are target words, and their values are
+        a list of their neighbors (e.g., the output of get_neighbor_dict()).
+    """
+    types = []
+    targets = neighbor_dict.keys()
+    while targets:
+        current = targets.pop()
+        for neighbor in neighbors:
+            if len(current) == len(neighbor): # if they are the same length, the change was a substitution
+                types.append((current, neighbor, "substitution"))
+            elif len(current) > len(neighbor): # if the target is longer than the neighbor, the change was a deletion
+                types.append((current, neighbor, "deletion"))
+            elif len(current) < len(neighbor): # if the target is shorter than the neighbor, the change was an addition
+                types.append((current, neighbor, "addition"))
+            else: # just to be sure
+                types.append((current, neighbor, "unknown"))
+    return types
