7e2bf987ef05111120a375265bcb2cd300b7ae3a
R Steiner <r.steiner@uconn.edu>
2017-09-27 12:46:03 -0400

    Start to turn get_types into a fnc (WIP).

--- lexlib/get-type.py
+++ lexlib/get-type.py
@@ -1,24 +1,21 @@
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
 
 #import sys
-import numpy as np
+# import numpy as np
 
 # given a file organized with lines containing a target word and a neighbor
-wordlist = np.loadtxt('types-test.txt',dtype=str)
+# wordlist = np.loadtxt('types-test.txt',dtype=str)
 #wordlist = sys.argv[1]
 
-types = []
-bad = []
-
-for i, j in wordlist: # where i is the target word and j is the neighbor
-    if len(i) == len(j): # if they are the same length, the change was a substitution
-        types.append([i,j,'S'])
-    elif len(i) > len(j): # if the target is longer than the neighbor, the change was a deletion
-        types.append([i,j,'D'])
-    elif len(i) < len(j): # if the target is shorter than the neighbor, the change was an addition
-        types.append([i,j,'A'])
-    else: # just to be sure
-        bad.append([i,j])
-
-np.savetxt('types.txt',types,fmt='%s')
-np.savetxt('bad.txt',bad,fmt='%s')
+def get_neighbor_types(neighbor_dict):
+    types = {}
+    base_words = neighbor_dict.keys()
+    for i, j in wordlist: # where i is the target word and j is the neighbor
+        if len(i) == len(j): # if they are the same length, the change was a substitution
+            types.append([i,j,'S'])
+        elif len(i) > len(j): # if the target is longer than the neighbor, the change was a deletion
+            types.append([i,j,'D'])
+        elif len(i) < len(j): # if the target is shorter than the neighbor, the change was an addition
+            types.append([i,j,'A'])
+        else: # just to be sure
+            bad.append([i,j])
