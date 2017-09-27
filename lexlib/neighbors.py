88510421d5350c49a6baec680fc366c3bc94e054
R Steiner <r.steiner@uconn.edu>
2017-09-27 13:45:33 -0400

    Move get_neighbor_types() to neighbors.py

--- lexlib/neighbors.py
+++ lexlib/neighbors.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
 """
-module: get_neighbor.py
-author: R Steiner
+module: neighbors.py
+author: R. Steiner
 license: MIT License, copyright (c) 2016 R Steiner
 description: Functions to find the phonological neighbors of a list of words.
 """
@@ -91,11 +91,37 @@ def get_neighbor_pairs(words, **kwargs):
             elif len(qsplit) == wlen+1:
                 neighbors.append((word, q)) if __check_addition(wsplit, qsplit) else None
             elif len(qsplit) == wlen-1:
-                neighbors.append((word, q) if __check_deletion(wsplit, qsplit) else None
+                neighbors.append((word, q)) if __check_deletion(wsplit, qsplit) else None
             else:
                 continue
     return neighbors
 
+
+def get_neighbor_types(neighbor_dict):
+    """
+    Takes a dict of neighbors (where a key is a "target" word and its value is a
+    list of all of its neighbors) and returns a list of (word1, word2,
+    relationship) triples, where `relationship` is one of "deletion,"
+    "addition," "substitution," or "unknown".
+
+    arguments:
+        neighbor_dict -- a dict whose keys are target words, and their values are
+        a list of their neighbors (e.g., the output of get_neighbor_dict()).
+    """
+    types = []
+    targets = neighbor_dict.keys()
+    while targets:
+        current = targets.pop()
+        for neighbor in neighbor_dict[current]:
+            if len(current) == len(neighbor): # if they are the same length, the change was a substitution
+                types.append((current, neighbor, "substitution"))
+            elif len(current) > len(neighbor): # if the target is longer than the neighbor, the change was a deletion
+                types.append((current, neighbor, "deletion"))
+            elif len(current) < len(neighbor): # if the target is shorter than the neighbor, the change was an addition
+                types.append((current, neighbor, "addition"))
+            else: # just to be sure
+                types.append((current, neighbor, "unknown"))
+    return types
 
 
 def __check_addition(base, candidate):
