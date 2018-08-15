c2015f90378ce28743e3a49e2c5dd6bd4b068c99
R Steiner <r.steiner@uconn.edu>
2018-08-15 15:04:48 -0400

    Include docs in Manifest.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ wd = path.abspath(path.dirname(__file__))
 with open(path.join(wd, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
-ver = "2.2.1"
+ver = "2.2.2"
 repo = "https://github.com/cranndarach/lexlib"
 setup(name="lexlib",
       version=ver,
@@ -17,11 +17,15 @@ setup(name="lexlib",
                 psychology cognitive science cogsci",
       url=repo,
       download_url="{}/tarball/{}".format(repo, ver),
+      project_urls={
+          "Documentation": "https://lexlib.readthedocs.io"
+     },
       author="R. J. Steiner",
       author_email="r.steiner@uconn.edu",
       license="MIT",
       packages=["lexlib"],
       classifiers=[
+          "Programming Language :: Python :: 3",
           "Development Status :: 4 - Beta",
           "Intended Audience :: Science/Research",
           "License :: OSI Approved :: MIT License"
