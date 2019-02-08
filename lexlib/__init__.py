2cdb1afc18afaeac125be1f539a65d7bd923d63b
R Steiner <r.steiner@uconn.edu>
2019-02-07 20:19:06 -0500

    Remove deprecated functions.

--- lexlib/__init__.py
+++ lexlib/__init__.py
@@ -4,7 +4,7 @@
 Set of utilities for research involving words.
 """
 
-# copyright: 2016-2018 R. Steiner
+# copyright: 2016-2019 R. Steiner
 # license: MIT License
 
 from .io import *
@@ -16,6 +16,3 @@ __all__ = ["check_neighbors", "get_words", "clusters", "clusters_word",
            "get_cv", "get_neighbor_dict", "get_neighbor_pairs",
            "get_neighbor_positions", "get_neighbor_types", "get_neighbors",
            "nsyll_list", "nsyll_word", "filter_by_nsyll", "filter_words"]
-
-get_neighbors = deprecation_decorator(get_neighbor_dict)
-filter_words = deprecation_decorator(filter_by_nsyll)
